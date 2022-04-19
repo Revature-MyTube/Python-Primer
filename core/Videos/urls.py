07bf67d7a08b48428f6854a96a8a7335099a6909
Brian Stiner <Brian.Stiner@revature.net>
2022-04-19 15:59:16 -0700

    um.. no 1st - url user sees 2nd - view function

--- core/Videos/urls.py
+++ core/Videos/urls.py
@@ -7,19 +7,10 @@ urlpatterns = [
     path("profile/<str:username>", views.profile, name="profile"),
     path("watch/<str:video_id>", views.watch, name="watch"),
 
-    path(<iframe width="420" height="315"
-     src="https://www.youtube.com/watch?v=TK6w8poGSkQ">
-     </iframe>),
-
-    path(<iframe width="420" height="315"
-     src="https://www.youtube.com/watch?v=BgIgKcqPd4k&t=43s">
-     </iframe>)
-
-    path(<iframe width="420" height="315"
-     src="https://www.youtube.com/watch?v=1zrtBqZeylo">
-     </iframe>)
-
     # path("channel/<str:channel_id>", views.view_channel, name="view_channel"),
     # path("playlist/<str:playlist_id>", views.view_playlist, name="view_playlist"),
     # path("search/<str:query>", views.search, name="search"),
+    # https://www.youtube.com/watch?v=BgIgKcqPd4k
+    # https://www.youtube.com/watch?v=TK6w8poGSkQ
+    # https://www.youtube.com/watch?v=1zrtBqZeylo
 ]
