53eeecdb9bab811eef1707e95fe0dec40d5a9dca
aharrah95 <abigail.harrah@revature.net>
2022-04-19 15:57:18 -0500

    Abby iframe #2, 3 video urls; Videos > urls.py > added iframe statement, "Unexpected expression syntax"

--- core/Videos/urls.py
+++ core/Videos/urls.py
@@ -7,12 +7,17 @@ urlpatterns = [
     path("profile/<str:username>", views.profile, name="profile"),
     path("watch/<str:video_id>", views.watch, name="watch"),
 
-    (<iframe width="420" height="315"
+    path(<iframe width="420" height="315"
      src="https://www.youtube.com/watch?v=TK6w8poGSkQ">
      </iframe>),
 
-    path("home/<https://www.youtube.com/watch?v=BgIgKcqPd4k&t=43s>", views.home, name="home"),
-    path("home/<https://www.youtube.com/watch?v=1zrtBqZeylo>", views.home, name="home"),
+    path(<iframe width="420" height="315"
+     src="https://www.youtube.com/watch?v=BgIgKcqPd4k&t=43s">
+     </iframe>)
+
+    path(<iframe width="420" height="315"
+     src="https://www.youtube.com/watch?v=1zrtBqZeylo">
+     </iframe>)
 
     # path("channel/<str:channel_id>", views.view_channel, name="view_channel"),
     # path("playlist/<str:playlist_id>", views.view_playlist, name="view_playlist"),
