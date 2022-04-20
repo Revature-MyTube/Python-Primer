7549b3b2da4a9003b1f6c46c5e3a341a1b2b0396
Brian Stiner <Brian.Stiner@revature.net>
2022-04-20 14:15:12 -0700

    admin migration changes

--- core/Videos/admin.py
+++ core/Videos/admin.py
@@ -4,18 +4,19 @@ from . import models
 
 class VideoAdmin(admin.ModelAdmin):
     list_display = ('name', 'views')
-
-
+    list_filter = ('name', 'views')
+    
 class ChannelAdmin(admin.ModelAdmin):
     list_display = ('name', 'views', 'owner')
-
+    list_filter = ('name', 'views', 'owner')
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('video', 'author', 'created_at')
-
+    list_filter = ('video', 'author', 'created_at')
 
 class PlaylistAdmin(admin.ModelAdmin):
     list_display = ('name', 'about', 'created_at')
+    list_filter = ('name', 'about', 'created_at')
 
 
 admin.site.register(models.Video, VideoAdmin)
