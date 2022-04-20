af74b49539f0431e2c5381bd26f762bc4e4168b3
Brian Stiner <Brian.Stiner@revature.net>
2022-04-20 09:26:44 -0700

    html does not belong in python

--- core/Videos/urls.py
+++ core/Videos/urls.py
@@ -8,34 +8,34 @@ urlpatterns = [
                   path("watch/<str:video_id>", views.watch, name="watch"),
 
 
-              < iframe;
-width = "1352";
-height = "624";
-src = "https://www.youtube.com/embed/TK6w8poGSkQ";
-title = "Zootopia 'Try Everything' Dance Workout";
-frameborder = "0";
-allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-allowfullscreen > < / iframe >
+#               < iframe;
+# width = "1352";
+# height = "624";
+# src = "https://www.youtube.com/embed/TK6w8poGSkQ";
+# title = "Zootopia 'Try Everything' Dance Workout";
+# frameborder = "0";
+# allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+# allowfullscreen > < / iframe >
 
 
-                      < iframe
-width = "1352";
-height = "624";
-src = "https://www.youtube.com/embed/BgIgKcqPd4k";
-title = "Kittens Meowing";
-frameborder = "0";
-allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-allowfullscreen > < / iframe >
+#                       < iframe
+# width = "1352";
+# height = "624";
+# src = "https://www.youtube.com/embed/BgIgKcqPd4k";
+# title = "Kittens Meowing";
+# frameborder = "0";
+# allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+# allowfullscreen > < / iframe >
 
 
-                      < iframe
-width = "1352";
-height = "624";
-src = "https://www.youtube.com/embed/1zrtBqZeylo";
-title = "Amazing Basketball Shots";
-frameborder = "0";
-allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-allowfullscreen > < / iframe >
+#                       < iframe
+# width = "1352";
+# height = "624";
+# src = "https://www.youtube.com/embed/1zrtBqZeylo";
+# title = "Amazing Basketball Shots";
+# frameborder = "0";
+# allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+# allowfullscreen > < / iframe >
 
 # path("channel/<str:channel_id>", views.view_channel, name="view_channel"),
 # path("playlist/<str:playlist_id>", views.view_playlist, name="view_playlist"),
